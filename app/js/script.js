4e3a6198ef802c0ad0034318a879985706f888b4
Øystein Håberg <islandstone89@gmail.com>
2024-10-29 00:20:42 +0100

    * Removed animation * Nested :has on body * Chenged :has selector from :checked state to attribute selector

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,16 @@
 
+/* let toggleDiv = document.getElementById(".introToggle"); */
 let toggle = document.getElementById("toggle");
 let toggleLabel = document.getElementById("toggleLabel");
 
 
 function toggleSwitch() {
-    if (toggleLabel.getAttribute("data-theme-mode") === "light") {
-        toggleLabel.setAttribute("data-theme-mode", "dark")
+    if (toggle.getAttribute("data-theme-mode") === "light") {
+        toggle.setAttribute("data-theme-mode", "dark")
     }
 
     else {
-        toggleLabel.setAttribute("data-theme-mode", "light")
+        toggle.setAttribute("data-theme-mode", "light")
     }
 }
 
